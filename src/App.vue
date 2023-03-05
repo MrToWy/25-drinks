7415070bbf4f42170691495c83791df829715e32
Tobias Wylega <tobias.wylega@stud.hs-hannover.de>
2023-03-05 15:00:17 +0100

    rm github & netlify link

--- src/App.vue
+++ src/App.vue
@@ -25,16 +25,6 @@
 
     <router-view />
 
-    <footer class="container mt-4 text-center">
-      <hr />
-      <div>
-        View the source or contribute a drink recipe on
-        <a href="https://github.com/alfg/opendrinks">GitHub</a>!
-      </div>
-      <a href="https://www.netlify.com">
-        <img src="https://www.netlify.com/img/global/badges/netlify-light.svg" alt="Netlify" />
-      </a>
-    </footer>
   </div>
 </template>
 
